411b59b16eb41f0289eb7b0863ddfcbc8e1ba1b7
Miguel Curbelo <mcurbeloj@gmail.com>
2022-11-01 21:10:12 +0100

    adds the basic schema for recipe

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: 'string', // required and unique
+  level: 'string', // has to include the enum, to only allow a few values
+  ingredients: [ String ],
+  cuisine: 'string', // required
+  dishType: 'string', // also only a few values possible
+  image: 'string', // include default image
+  duration: 0, // minimum is 0
+  creator: 'string',
+  created: Date, // by default, today
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
